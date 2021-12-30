cb0cc5b42d51157665f0776200eec862efb40842
yadess <90118673+yadessom@users.noreply.github.com>
2021-12-30 14:06:59 +0900

    ~ footer

--- js/main.js
+++ js/main.js
@@ -1,6 +1,39 @@
 $(function(){
 
-    
+    $('.m_btn').click(function(){
+        $('.line').toggleClass('on');
+        $('.m_menu').toggleClass('on');
+    });    
+    // *****************************************
+    $('.sec_02_slide').slick({
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        adaptiveHeight: true
+    });
+    // *****************************************
+    $('.sec_03_slide').slick({
+        infinite: false,
+        arrows:false,
+        dots:true,
+        slidesToShow: 3,
+        slidesToScroll: 2
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
